13cd21710d5539e944256858b29c23fda54aa776
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-05-15 17:56:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Footer, Header } from './components';
+import { AllRoutes } from './Routes/AllRoutes';
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      <AllRoutes/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
