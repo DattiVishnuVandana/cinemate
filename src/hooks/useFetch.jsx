eb877c5b1b46106d2b691d363b1a2e9ca52a8c57
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-05-19 18:43:05 +0530

    useSearchParams

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-export const useFetch = (apiPath) => {
-const url=`https://api.themoviedb.org/3/${apiPath}?api_key=b80d59c33d6d57ed9c7e3713f91c188a`
+export const useFetch = (apiPath,queryTerm="") => {
+const url=`https://api.themoviedb.org/3/${apiPath}?api_key=b80d59c33d6d57ed9c7e3713f91c188a&query=${queryTerm}`
 const [data,setData]=useState([])
      useEffect(()=>{
         async function fetchMovies() {
