5e7827bd805c1c8747fa58691a03f2d4afe69abd
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-05-19 22:37:30 +0530

    ScrollToTop-useLocation()

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 export {Header} from "./Header";
 export {Footer} from "./Footer";
-//
+
 export {Card} from "./Card"
+
--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 import {BrowserRouter as Router} from "react-router-dom";
 import App from "./App.jsx";
+
+import {ScrollToTop} from "./components/ScrollToTop.jsx"
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Router>
+            <ScrollToTop/>
             <App/>
         </Router>
         
